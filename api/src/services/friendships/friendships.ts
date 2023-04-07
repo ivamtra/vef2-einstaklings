4f38b53d85c009c99e3845b8c3c9361bc6968d0f
ivamtra <97352752+ivamtra@users.noreply.github.com>
2023-04-07 20:48:32 +0000

    endpoints...

--- api/src/services/friendships/friendships.ts
+++ api/src/services/friendships/friendships.ts
@@ -10,6 +10,16 @@ export const friendships: QueryResolvers['friendships'] = () => {
   return db.friendship.findMany()
 }
 
+export const friendshipsByUser: QueryResolvers['friendships'] = ({
+  userId,
+}: any) => {
+  return db.friendship.findMany({
+    where: {
+      userId1: userId,
+    },
+  })
+}
+
 export const friendship: QueryResolvers['friendship'] = ({ id }) => {
   return db.friendship.findUnique({
     where: { id },
--- api/src/services/posts/posts.ts
+++ api/src/services/posts/posts.ts
@@ -10,6 +10,11 @@ export const posts: QueryResolvers['posts'] = () => {
   return db.post.findMany()
 }
 
+// TODO
+export const postsByFriends: QueryResolvers['posts'] = ({ userId }: any) => {
+  return []
+}
+
 export const post: QueryResolvers['post'] = ({ id }) => {
   return db.post.findUnique({
     where: { id },
