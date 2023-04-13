28d3525ab19de92c550c22cb492b5cc7a88a0f1f
ivamtra <97352752+ivamtra@users.noreply.github.com>
2023-04-13 22:11:59 +0000

    Stable client

--- api/db/migrations/20230412170556_unique_reciever_og_sender_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[recieverId,senderId]` on the table `FriendRequest` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "FriendRequest_recieverId_senderId_key" ON "FriendRequest"("recieverId", "senderId");
